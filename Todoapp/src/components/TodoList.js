5be418e2a45d8ede8a7ae785b8e17fcdf724e1f3
Amandeep <amandeepsingh57537@gmail.com>
2024-04-06 23:08:55 +0530

    add Todoapp

---
+++ Todoapp/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoList(props) {
+  return (
+    <li>
+      {props.item}
+      <span>
+        <i className="fa-solid fa-delete-left" onClick={(e) => {props.deleteItem(props.index)}} />
+      </span>
+    </li>
+  );
+}
+
+export default TodoList;
