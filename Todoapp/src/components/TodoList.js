5a251b7279a4227cb52c9d3e05f9b3fad562a211
Amandeep <amandeepsingh57537@gmail.com>
2024-05-05 23:17:43 +0530

    Todo app

--- Todoapp/src/components/TodoList.js
+++ Todoapp/src/components/TodoList.js
@@ -3,6 +3,7 @@ import React from "react";
 function TodoList(props) {
   return (
     <li>  
+      
       {props.item}
       <span>
         <i className="fa-solid fa-delete-left" onClick={(e) => {props.deleteItem(props.index)}} />
