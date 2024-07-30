189eb942d4d1210d7c6117c8e3060710394eb396
Amandeep <amandeepsingh57537@gmail.com>
2024-07-30 13:21:44 +0530

    change name 03tailwindprops

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
-
-import "./App.css";
-
 function App() {
-  let [counter, setCounter] = useState(12);
-  // let counter = 12;
+  const [counter, setCounter] = useState(12);
+
   const addvalue = () => {
     // counter = counter + 1;
-    setCounter((prevcounter) => prevcounter + 1);
-    setCounter((prevcounter) => prevcounter + 1);
-    setCounter((prevcounter) => prevcounter + 1);
-    setCounter((prevcounter) => prevcounter + 1);
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    console.log("clicked", counter);
+    setCounter(counter + 1);
   };
   const removevalue = () => {
-    // console.log("clicked", counter);
-    // counter = counter - 1;
     setCounter(counter - 1);
   };
   return (
     <>
-      <h1>hello babutech</h1>
-      <h2>counter value:{counter}</h2>
-
-      <button onClick={addvalue}>Add Value {counter}</button>
-      <br />
-      <button onClick={removevalue}>Remove Value {counter}</button>
+      <div>
+        <h1 style={{ textAlign: "center" }}>Counter Project using usestate</h1>
+        <button onClick={addvalue}>Add value: {counter}</button>
+        <br />
+        <button onClick={removevalue}> Remove value : {counter}</button>
+      </div>
     </>
   );
 }
