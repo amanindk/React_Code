03d78ff914700ed25e401bbb73548e661d068344
Amandeep <amandeepsingh57537@gmail.com>
2024-11-07 22:07:49 +0530

    components files

---
+++ 12MegaBlog/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button({
+    children,
+    textColor: 'text-white',
+    bgColor:'bg-blue-600',
+    type:'button',
+    classname: '',
+    ...props
+}) {
+  return(
+     <button className={`px-4 py-2 rounded-lg ${textColor} ${bgColor} ${className}`} {...props}>{children}</button>
+    )
+}
+export default Button;
